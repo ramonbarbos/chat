a7f756be0a811a86953140664f5fa08bf245d377
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-03-23 21:12:55 -0300

    atualizar quantidade produto

--- painel/pages/visualizar-produtos.php
+++ painel/pages/visualizar-produtos.php
@@ -3,8 +3,24 @@
 
     <div class="container" style="border-radius: 7px; background-color: rgb(250, 250, 250);  align-items: center;justify-content: center; padding: 40px; width: 800px;">
 
+    <?php
+                if(isset($_POST['atualizar'])){
 
-                <form style="width: 600px;" method="post" enctype="multipart/form-data">
+                    $quantidade = $_POST['quantidade'];
+                    $produto_id = @$_POST['produto_id'];
+                    if($quantidade <= 0){
+                        Painel::alerta('erro','Erro ao atualizar a quantidade');
+
+                    }else{
+                        $sql = MySql::conectar()->prepare("UPDATE `tb_admin.estoque` SET quantidade = $quantidade WHERE id = $produto_id ");
+                        $sql->execute();
+
+                        Painel::alerta('sucesso','Produtos atualizado com sucesso');
+
+                    }
+                }
+                ?>
+                <form style="width: 600px;" method="post" enctype="multipart/form-data">  
 
                 <div class="mb-3 d-flex">
                     <input type="text"  class="form-control" name="buscar" placeholder="Pesquisar">
@@ -12,28 +28,47 @@
                 </div>
                 </form>
 
-                <div class="container-card">
+                <div class="container" style="display:flex">
 
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">Nome do produto: </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">Descrição: </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">Largura: </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">Altura: </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">Comprimento: </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">Peso: </h6>
-                        <h6 class="card-subtitle mb-2 text-muted ">Quantidade: </h6>
-                        <div class="mb-3 d-flex">
-                          <input type="number" name="quantidade" min="0" max="900" step="1" class="form-control" value="0" >
-                          <input type="submit" class="btn btn-outline-dark" value="Atualizar" name="atualizar">
+                <?php $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.estoque`");
+                       $sql->execute();
+                       $produtos = $sql->fetchAll();
+                       foreach( $produtos as $key => $value){ 
+                        $imagemSingle = MySql::conectar()->prepare("SELECT * FROM `tb_admin.estoque_imagens` WHERE produto_id = $value[id] LIMIT 1");
+                        $imagemSingle->execute();
+                        $imagemSingle = $imagemSingle->fetch()['imagem'];
+                ?>
 
+                
+                
+
+                    <div class="card d-flex" style="width: 18rem; margin-right: 5px;">
+
+                        <div class="">
+                            <img src="<?php echo INCLUDE_PATH_PAINEL ?>uploads/<?php echo $imagemSingle ?>" class="card-img-top" alt="...">
                         </div>
-                        <a href="#" class="card-link">Editar</a>
-                        <a href="#" class="card-link">Excluir</a>
-                    </div>
+                            <div class="card-body">
+                                <h6 class="card-subtitle mb-2 text-muted">Nome do produto: <?php echo $value['nome'] ?> </h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Descrição: <?php echo $value['descricao'] ?>  </h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Largura: <?php echo $value['largura'] ?> </h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Altura: <?php echo $value['altura'] ?>  </h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Comprimento: <?php echo $value['comprimento'] ?>  </h6>
+                                <h6 class="card-subtitle mb-2 text-muted">Peso: <?php echo $value['peso'] ?>  </h6>
+                                <h6 class="card-subtitle mb-2 text-muted ">Quantidade: </h6>
+                                <form method="post" >
+                                    <div class="mb-3 ">
+                                        <input type="number" name="quantidade" min="0" max="900" step="1" class="form-control" value="<?php echo $value['quantidade'] ?>" >
+                                        <input type="hidden" name="produto_id" value="<?php echo $value['id'] ?>" >
+                                        <input type="submit" class="btn btn-outline-dark" value="Atualizar" name="atualizar">
+                                    </div>
+                                </form>
+                                <a  class="card-link">Editar</a>
+                                <a item_id="<?php echo $value['id'] ?>" class="card-link">Excluir</a>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
 
-                </div>
 
     </div>
 
